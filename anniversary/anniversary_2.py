2d7ded4b90bddf75c0d891b5ffe56ba2a1815d6c
crocus7125 <36184489+crocus7125@users.noreply.github.com>
2025-05-12 12:18:44 +0000

    complete

--- anniversary/anniversary_2.py
+++ anniversary/anniversary_2.py
@@ -1,14 +1,11 @@
 from datetime import datetime
 
 now = datetime.now()
-wedding_day = datetime(2017, 7, 22)
-roa_birth = datetime(2018, 5, 7)
-ina_birth = datetime(2019, 7, 27)
+wedding_day = datetime(2011, 10, 1)
+ubin_birth = datetime(2012, 11, 27)
 
 wedding_days = now - wedding_day
-roa_days = now - roa_birth
-ina_days = now - ina_birth
+ubin_days = now - ubin_birth
 
 print(f"우리 결혼한지 +{wedding_days.days}일째")
-print(f"로아 태어난지 +{roa_days.days}일째")
-print(f"인아 태어난지 +{ina_days.days}일째")
+print(f"우빈 태어난지 +{ubin_days.days}일째")
